b6d06f7c47d8d924359f539f4b9e788e1b8213b3
Iulia Bivolaru <ibivolaru@norauto.com>
2018-10-22 13:41:28 +0200

    Create tabs

--- main.js
+++ main.js
@@ -1,46 +1,54 @@
-Vue.component('task-list', {
-    template: '<div><task v-for="task in tasks">{{ task.description }}</task></div>',
+Vue.component('tabs', {
+    template: `
+<div>    
+<div class="tabs">
+    <ul>
+        <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+            <a :href="tab.href" @click="selectTab(tab)">{{tab.name}}</a></li>
+        </li>
+    </ul>
+</div>
+  
+<div class="tab-details">
+    <slot></slot>
+</div>
+</div>`,
 
     data() {
-        return {
-            tasks: [
-                { description: 'Go to the store', complete: true },
-                { description: 'Go to the email', complete: false },
-                { description: 'Go to the farm', complete: true },
-                { description: 'Go to work', complete: false }
-            ]
+        return { tabs: [] };
+    },
+
+    created() {
+        this.tabs = this.$children;
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name === selectedTab.name);
+            });
         }
     }
-});
-
-Vue.component('task', {
-    template: '<li><slot></slot></li>'
-});
-
-Vue.component('message', {
-    props: ['title', 'body'],
-    template: `<article class="message">
-                    <div class="message-header">
-                        {{title}}
-                    </div>
-
-                    <div class="message-body">
-                        {{body}}
-                    </div>
-               </article>`
 })
 
-Vue.component('modal', {
-    template: `<div class="modal is-active">
-            <div class="modal-background"></div>
-            <div class="modal-content">
-                <div class="box">
-                    <slot></slot>
-                </div>
-            </div>
-            <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
-        </div>`
-})
+Vue.component('tab', {
+    template: `<div v-show="isActive"><slot></slot></div>`,
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+    data() {
+        return { isActive: false };
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+    mounted() {
+        this.isActive = this.selected;
+    }
+});
 
 new Vue({
     el: '#root',
