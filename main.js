6c8ab3a6084f0055617f74240c51863d5226deab
Iulia Bivolaru <ibivolaru@norauto.com>
2018-10-22 10:40:43 +0200

    Create modal

--- main.js
+++ main.js
@@ -30,7 +30,22 @@ Vue.component('message', {
                </article>`
 })
 
+Vue.component('modal', {
+    template: `<div class="modal is-active">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+                <div class="box">
+                    <slot></slot>
+                </div>
+            </div>
+            <button class="modal-close is-large" @click="$emit('close')" aria-label="close"></button>
+        </div>`
+})
+
 new Vue({
-    el: '#root'
+    el: '#root',
+    data: {
+        showModal: false
+    }
 })
 
