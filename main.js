648462c371c0eb6748acd3a317e1f9c58a863a93
Iulia Bivolaru <ibivolaru@norauto.com>
2018-10-22 15:25:12 +0200

    Practice component communication

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+window.Event = new Vue();
+
 Vue.component('coupon', {
     template: `<input placeholder="Enter your coupon code" @blur="onCouponApplied">`,
 
     methods: {
         onCouponApplied() {
-            this.$emit('applied')
+            Event.$emit('applied');
         }
     }
 });
@@ -13,10 +15,8 @@ new Vue({
     data: {
         couponApplied: false
     },
-    methods: {
-        onCouponApplied() {
-            this.couponApplied = true;
-        }
+    created() {
+        Event.$on('applied',  () => alert('Handling it'));
     }
 })
 
